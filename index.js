fde0c9bf02e2e6b591972bb8be765427515c4fe7
da3491 <87324456+da3491@users.noreply.github.com>
2022-06-13 14:08:06 -0400

    Changed HTTP request to AJAX

--- index.js
+++ index.js
@@ -8,62 +8,113 @@
 $(document).ready(function () {
   console.log("Document loaded");
 
-  var httpRequest = new XMLHttpRequest();
-  httpRequest.onload = function () {
-    if (httpRequest.readyState === XMLHttpRequest.DONE) {
-      if (httpRequest.status === 200) {
-        // console.log(JSON.parse(httpRequest.responseText)["tasks"]);
-        renderData(JSON.parse(httpRequest.responseText)["tasks"]);
-      } else {
-        console.log(httpRequest.statusText);
-      }
-    }
-  };
-  httpRequest.onerror = function () {
-    console.log(httpRequest.statusText);
-  };
-  httpRequest.open(
-    "GET",
-    "https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=426"
-  );
-  httpRequest.send(null);
-
-  var renderData = function (data) {
-    //   Use to loop over data array and display
-    data.forEach(function (e) {
-      var itemName = e.content;
-      $("#list").append(
-        "<div class='flex-grow-1 item'>" +
-          "<input type='checkbox' />" +
-          "<span class='item-name'>" +
-          itemName +
-          "</span>" +
-          "</div>" +
-          "<button class='btn btn-danger'>delete</button>" +
-          "</div>"
-      );
+  // get initial JSON
+  var renderData = function () {
+    $("#list").empty();
+    $.ajax({
+      type: "GET",
+      url: "https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=426",
+      dataType: "json",
+      success: function (response, textStatus) {
+        console.log("Loaded: " + textStatus);
+        //   Use to loop over data array and display
+        response.tasks.forEach(function (item) {
+          if (item.completed == false) {
+            isChecked = "";
+          } else isChecked = "checked";
+          console.log(item);
+          $("#list").append(
+            "<div class='row row-cols-auto align-items-center py-2'>" +
+              "<div class='flex-grow-1 item'>" +
+              "<input type='checkbox' class='check-item' />" +
+              "<span data-id='" +
+              item.id +
+              "' class='item-name'>" +
+              item.content +
+              "</span>" +
+              "</div>" +
+              "<button class='btn btn-danger remove'>delete</button>" +
+              "</div>"
+          );
+        });
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      },
     });
   };
 
   // add item
-  $("#add-item button").click(function () {
+  $("#add-item").on("click", "button", function () {
     console.log("clicked");
     var itemName = $(".input-entry").val();
-    httpRequest.open(
-      "POST",
-      "https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=426"
-    );
-    httpRequest.setRequestHeader("Content-Type", "application/json");
-    var newTask = JSON.stringify({
-      task: {
-        content: itemName,
-        completed: false,
+    //AJAX request here
+    $.ajax({
+      type: "POST",
+      url: "https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=426",
+      contentType: "application/json",
+      dataType: "json",
+      data: JSON.stringify({
+        task: {
+          content: itemName,
+        },
+      }),
+      success: function (response, textStatus) {
+        console.log(response);
+        renderData();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
       },
     });
-    httpRequest.send(newTask);
-    //
+    $(".input-entry").val("");
   });
 
   // remove item
+  $("#list").on("click", "button.remove", function () {
+    // Note: Had an issue targetting dynamic elements after added. Solution was to target the parent element that holds the added items, and then to select the correct element as an argument.
+    var itemId = $(this).prev().find(".item-name").data("id");
+    console.log(itemId);
+    $.ajax({
+      type: "DELETE",
+      url:
+        "https://altcademy-to-do-list-api.herokuapp.com/tasks/" +
+        itemId +
+        "?api_key=426",
+      success: function (response, textStatus) {
+        console.log("Delete: " + response);
+        renderData();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      },
+    });
+  });
+
   // all checked items are crossed out
+  $("#list").on("click", ".check-item", function () {
+    var checked = $(this).prop("checked");
+    var itemId = $(this).next().data("id");
+    $.ajax({
+      type: "PUT",
+      url:
+        "https://altcademy-to-do-list-api.herokuapp.com/tasks/" +
+        itemId +
+        "?api_key=426",
+      contentType: "application/json",
+      dataType: "json",
+      data: JSON.stringify({
+        //change content here
+        completed: checked,
+      }),
+      success: function (response, textStatus) {
+        console.log(textStatus);
+        renderData();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      },
+    });
+  });
+  renderData();
 });
